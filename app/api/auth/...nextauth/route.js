e258c82cd10941048c0e76a7be3dc1534947ae12
ramizPro <ozbej07@gmail.com>
2025-11-24 11:53:38 +0100

    added bcryptjs

--- app/api/auth/...nextauth/route.js
+++ app/api/auth/...nextauth/route.js
@@ -2,7 +2,7 @@
 
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const users = []; // replace with DB
 
