c4b021d5695a29f6379fdabe8b7e6bcd6ae1f4bb
ramizPro <ozbej07@gmail.com>
2025-12-09 12:20:11 +0100

    backgroun fixed, nasllov style

--- app/mainPage/page.tsx
+++ app/mainPage/page.tsx
@@ -31,8 +31,14 @@ export default function Home() {
   return (
     <main
       className="min-h-screen bg-gray-100"
-      style={{ backgroundImage: "url('/background.png')" }}
+      style={{
+        backgroundImage: "url('/background.png')",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
     >
+
       <div className="bg-black/60 shadow p-6 flex flex-col md:flex-row justify-between text-align-center">
         <h1 className="header_title">
           AgroTrg - Kupujte in prodajajte živino na spletu
@@ -96,8 +102,10 @@ export default function Home() {
         </aside>
 
         <section className="flex-1">
-          <h2 className="text-2xl font-bold mb-4">Vsi oglasi</h2>
-
+          <h2 style={{ WebkitTextStroke: "1px black" }}
+            className="text-white text-6xl font-bold mb-10 drop-shadow-lg">
+            Vsi oglasi
+          </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {oglasi.map((oglas) => (
               <div key={oglas._id} className="bg-white rounded-xl shadow p-4 hover:scale-105 transition-transform">
