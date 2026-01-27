cf3636d63c29a5becfd00af90d2e5d79d9f19972
Jevrec <185940139+Jevrec@users.noreply.github.com>
2026-01-27 01:19:02 +0100

    Bug #1
    Prvi bug, ki sem ga našel je bil, da gumb izbriši nima onClick, ali pa Delete (3/10), na koncu sem ugotovil,da je bila to samo neka palceholder stran, ki sem jo potem izbrisal (2/10).

--- app/oglasi/[id]/Actions.tsx
+++ app/oglasi/[id]/Actions.tsx
@@ -23,15 +23,6 @@ export default function OglasActions({ id }: { id: string }) {
 
   return (
     <div className="flex gap-4 mt-6">
-        {/*
-      <button
-        onClick={() => router.push(`/oglasi/${id}/edit`)}
-        className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600"
-      >
-        Uredi
-      </button>
-        
-      */}
       <button
         onClick={handleDelete}
         className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700"
