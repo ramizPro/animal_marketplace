299ee012f99e8353af89be3d6ee29fc44cec3495
Jevrec <185940139+Jevrec@users.noreply.github.com>
2026-01-27 02:30:38 +0100

    Bug #3 - SessionProvider
    Spletna stran sploh neve če imaš ti kakšno sejo ali ne (3/10) .Zato sem vse dodal session provider (3/10)

---
+++ app/components/SessionProvider.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+
+/**
+ * Bug 3#: SessionProvider wrapper
+*/
+export function SessionProvider({ children }: { children: React.ReactNode }) {
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+}
