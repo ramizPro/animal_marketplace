576693f1c1d1af2a1a64c8cdef4ce8435425ee1b
Jevrec <185940139+Jevrec@users.noreply.github.com>
2026-01-27 08:02:07 +0100

    Fix #2

--- app/components/HeaderHeaderMain.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { HeaderMain } from './Header'
-
-describe('<HeaderMain />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<HeaderMain />)
-  })
-})
