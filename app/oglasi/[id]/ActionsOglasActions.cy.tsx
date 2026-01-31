d883e760390720d53788de012414ee8dd131649c
Jevrec <185940139+Jevrec@users.noreply.github.com>
2026-01-31 20:55:46 +0100

    FIx #7

--- app/oglasi/[id]/ActionsOglasActions.cy.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import OglasActions from './Actions'
-
-describe('<OglasActions />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<OglasActions />)
-  })
-})
